9df2d1558eecbee49a4650a282f2eb2f206dd181
Two <605222844@qq.com>
2016-07-23 10:18:10 +0800

    Update store.

--- Application/Home/Controller/StoreController.class.php
+++ Application/Home/Controller/StoreController.class.php
@@ -33,6 +33,7 @@ class StoreController extends BaseController {
         
         $array = array ('code' => $code, 'message' => $message, 
             'result' => array (
+                'version' => $pullVersion['store_version'],
                 'product_min_id' => $productMinId,
                 'products' => $productList));
         
@@ -66,6 +67,73 @@ class StoreController extends BaseController {
                 'version' => $pullVersion['store_version'],
                 'product_min_id' => $productMinId,
                 'products' => $productList));
+        echo json_encode($array);
+    }
+    
+    public function refreshReward() {
+        $code = "10000";
+        $message = "刷新兑换奖品列表成功！";
+        
+        $version = $_POST['version'];
+        $rewardMinId = $_POST['reward_min_id'];
+        
+        $versionDao = M("pull_version");
+        $pullVersion = $versionDao->find();
+        
+        if(isset($version) && isset($rewardMinId)) {
+            $rewardDao = M("reward");
+            $condition['version'] = array('gt',$version);
+            $condition['pk_id'] = array('egt',$rewardMinId);
+            $rewardList = $rewardDao->where($condition)->order('pk_id desc')->limit(20)->select();
+        } else {
+            $code = "10008";
+            $message = "版本号或者记录最小ID值为空";
+        }
+        
+        if($rewardList != null && count($rewardList) > 0) {
+            $tempRewardMinId = end($rewardList)['pk_id'];
+            
+            if($tempRewardMinId < $rewardMinId) {
+                $rewardMinId = $tempRewardMinId;
+            }
+        }
+        
+        $array = array ('code' => $code, 'message' => $message, 
+            'result' => array (
+                'version' => $pullVersion['reward_version'],
+                'reward_min_id' => $rewardMinId,
+                'reward' => $rewardList));
+        
+        echo json_encode($array);
+    }
+    
+    public function getReward() {
+        $code = "10000";
+        $message = "获得兑换奖品列表成功！";
+        
+        $version = $_POST['version'];
+        $rewardMinId = $_POST['reward_min_id'];
+        
+        $versionDao = M("pull_version");
+        $pullVersion = $versionDao->find();
+        
+        if(isset($version) && isset($rewardMinId)) {
+            $rewardDao = M("reward");
+            $condition['version'] = array('elt',$version);
+            $condition['pk_id'] = array('lt',$rewardMinId);
+            $rewardList = $rewardDao->where($condition)->order('pk_id desc')->limit(20)->select();
+        } else {
+            $code = "10008";
+            $message = "版本号或者记录最小ID值为空";
+        }
+        
+        $array = array (
+            'code' => $code, 
+            'message' => $message, 
+            'result' => array (
+                'version' => $pullVersion['reward_version'],
+                'reward_min_id' => $rewardMinId,
+                'reward' => $rewardList));
         echo json_encode($array);
     }
     
