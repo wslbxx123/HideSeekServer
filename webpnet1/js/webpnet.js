62a278c6a6b5b847dd8baa6f83ccb4d9b6083266
xiangmin123 <270487268@qq.com>
2016-08-01 09:33:54 +0800

    change:

--- webpnet1/js/webpnet.js
+++ webpnet1/js/webpnet.js
@@ -10,6 +10,7 @@ function myStart(){
 			dataType: "json",
 			success: function(result, status) {
 				z = result.result.products.length;
+				alert(z);
 				for(var i = 0;i < result.result.products.length;i++){	
 					//创建商品橱窗框
 					var purArea = document.getElementById("purArea");
