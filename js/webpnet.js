e2fa7203ea725739b0cd88b75abc73d74b762aec
xiangmin123 <270487268@qq.com>
2016-08-05 16:01:37 +0800

    change

--- js/webpnet.js
+++ js/webpnet.js
@@ -99,6 +99,8 @@ function myStart(){
 					   		$(".goodsName").html(result.result.products[t].product_name);
 					   		alert(result.result.products[t].product_name);
 					   		$(".goodsprice").html($(".goodsNum").val*result.result.products[t].price+"元");
+					   		alert($(".goodsNum").val);
+					   		alert(result.result.products[t].price);
 					   		alert($(".goodsNum").val*result.result.products[t].price+"元");
 					   		$("#confirmpurchase").fadeIn();
 					   }
