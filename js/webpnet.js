b9c8bb614e3082706b3f8b59edb609dcb2ea3450
xiangmin123 <270487268@qq.com>
2016-08-05 17:50:03 +0800

    change

--- js/webpnet.js
+++ js/webpnet.js
@@ -298,6 +298,8 @@ function myStart(){
 				switch(result["code"]){
 					case "10000":
 						if(result["result"]["error_code"] == 0){
+							alert($("#codeNum").val());
+							alert(result["result"]["sms_code"]);
 							if($("#codeNum").val() == result["result"]["sms_code"]){
 							   codeNumber = true;
 							}
