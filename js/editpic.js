cb178b75440973b1dfba2d4da683f3a85c3df3c3
xiangmin123 <270487268@qq.com>
2016-08-16 10:58:14 +0800

    change

--- js/editpic.js
+++ js/editpic.js
@@ -98,52 +98,52 @@ document.getElementById("matchId").onclick = function(){
 	
 	
 	
-//	var index=document.getElementById("sex").selectedIndex;
-//	var data = "phone=" + document.getElementById("userphone").value 
-//				+ "&nickname="+ document.getElementById("userName").value
-//				+ "&password="+ document.getElementById("passwd1").value
-//				+ "&sex="+ document.getElementById("sex").options[index].text
-//				+ "&region=" + document.getElementById("citySelect").value
-//				+ "&role=" + myId
-//				+ "&photo_url=" + encodeURIComponent(document.getElementById("photo").src);
-//				
-//	var mymessages = {
-//		
-//		url: "/index.php/home/user/register",	
-//		type: 'POST',
-//		data: data,
-//		dataType: "json",
-//		
-////		jsonp: 'callback',
-////		jsonpCallback:"success_jsonpCallback",
-//		success: function(result, status) {
-//			alert(document.getElementById("photo").src);
-//			alert(JSON.stringify(result));
-//			switch(result["code"]){
-//				case "10000":
-//					Num = result["result"]["record"];
-//			        sessionid = result["result"]["session_id"];
-//			        $("#myorder").fadeIn(); 
-//			        getClick = true;
-//			        //存储注册数据
-//			  		sessionStorage.setItem("nickname", $("#userName").val());
-//					sessionStorage.setItem("record", Num);
-//					sessionStorage.setItem("myimgpath", $("#photo").attr("src"));
-//					sessionStorage.setItem("sessionid", result["result"]["session_id"]);
-//					$("#nickname").html($("#userName").val());
-//					$("#scoreNum").html(Num);
-//					$("#myimg").attr('src',$("#photo").attr("src")); 
-//					break;
-//			  	case "10003":
-//			  		alert("填写信息失败！")
-//			  		break;
-//			}	
-//		},
-//		error: function(XMLHttpRequest, textStatus, errorThrown) {
-//			alert("网络出现问题！");
-//		}
-//	};
-//	$.ajax(mymessages);		
+	var index=document.getElementById("sex").selectedIndex;
+	var data = "phone=" + document.getElementById("userphone").value 
+				+ "&nickname="+ document.getElementById("userName").value
+				+ "&password="+ document.getElementById("passwd1").value
+				+ "&sex="+ document.getElementById("sex").options[index].text
+				+ "&region=" + document.getElementById("citySelect").value
+				+ "&role=" + myId
+				+ "&photo_url=" + encodeURIComponent(document.getElementById("photo").src);
+				
+	var mymessages = {
+		
+		url: "/index.php/home/user/register",	
+		type: 'POST',
+		data: data,
+		dataType: "json",
+		
+//		jsonp: 'callback',
+//		jsonpCallback:"success_jsonpCallback",
+		success: function(result, status) {
+			alert(document.getElementById("photo").src);
+			alert(JSON.stringify(result));
+			switch(result["code"]){
+				case "10000":
+					Num = result["result"]["record"];
+			        sessionid = result["result"]["session_id"];
+			        $("#myorder").fadeIn(); 
+			        getClick = true;
+			        //存储注册数据
+			  		sessionStorage.setItem("nickname", $("#userName").val());
+					sessionStorage.setItem("record", Num);
+					sessionStorage.setItem("myimgpath", $("#photo").attr("src"));
+					sessionStorage.setItem("sessionid", result["result"]["session_id"]);
+					$("#nickname").html($("#userName").val());
+					$("#scoreNum").html(Num);
+					$("#myimg").attr('src',$("#photo").attr("src")); 
+					break;
+			  	case "10003":
+			  		alert("填写信息失败！")
+			  		break;
+			}	
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("网络出现问题！");
+		}
+	};
+	$.ajax(mymessages);		
 	
 }
 
