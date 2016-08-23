255ead4d36d8d2e0bd11f0e292c3ff7329b5f2aa
xiangmin123 <270487268@qq.com>
2016-08-23 16:44:12 +0800

    change

--- js/editpic.js
+++ js/editpic.js
@@ -29,8 +29,8 @@ $('#mycamera1').change(function(){
 	$("#newWin3").fadeIn(); 
 	$("#dataArea").fadeIn(); 
 	var fileimg = document.getElementById("fileimg");
-	var mycamera = document.getElementById('mycamera1');
-	getPath(fileimg,mycamera,fileimg);
+	var mycamera1 = document.getElementById('mycamera1');
+	getPath(fileimg,mycamera1,fileimg);
 });
 
 
@@ -586,7 +586,22 @@ function getPath(obj,fileQuery,transImg){
 			var images = new Image();
 			images.src = mypicture.toDataURL("image/jpeg");
 			$(".photo").attr("src",images.src);
-		}			
+		}	
+		
+		refreshData.onclick = function(){
+				if(n>0){
+					cover.drawImage(fileimg,0.1*n*(x-1)*y,0,coverpic.height*y,coverpic.height*y,0,0,500,500);
+					$("#newWin3").fadeOut(); 
+				}
+				else{
+					cover.drawImage(fileimg,0,0.1*(-n)*(x-1)*y,coverpic.width*y,coverpic.width*y,0,0,500,500);
+					$("#newWin3").fadeOut(); 
+				}
+			
+			var images = new Image();
+			images.src = mypicture.toDataURL("image/jpeg");
+			$(".photo").attr("src",images.src);
+		}		
 	});
 }			
 
