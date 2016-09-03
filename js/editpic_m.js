2894acf4f137dbac76dc3c46349c7aeafeaea031
xiangmin123 <270487268@qq.com>
2016-09-03 12:39:02 +0800

    change

--- js/editpic_m.js
+++ js/editpic_m.js
@@ -12,11 +12,9 @@ var data;
 //头像上传处理
 $('#mycamera').change(function(e){
 	
-	EXIF.getData(e.target.files[0], function() {
-		alert(EXIF.pretty(this));  
+	EXIF.getData(e.target.files[0], function() { 
          EXIF.getAllTags(this); 
          Orientation = EXIF.getTag(this,'Orientation'); 
-//       alert(Orientation);
     });
     
 	$("#newWin3").fadeIn(); 
