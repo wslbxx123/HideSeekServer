bc5b76ec23797081162c8102f826eb90b7774bde
xiangmin123 <270487268@qq.com>
2016-09-07 23:21:42 +0800

    change

--- js/webpnet_m.js
+++ js/webpnet_m.js
@@ -277,8 +277,8 @@ $(function(){
 				   		$("#confirmpay").click(function(){
 				   			alert($("#scoreNum").html());
 				   			alert(gNum);
-				   			if($(parseInt("#scoreNum").html())>=parseInt(gNum)){
-				   				$("#scoreNum").html($(parseInt("#scoreNum").html())-parseInt(gNum));
+				   			if(parseInt($("#scoreNum").html())>=parseInt(gNum)){
+				   				$("#scoreNum").html(parseInt($("#scoreNum").html())-parseInt(gNum));
 				   				var data = "session_id=" + sessionStorage.getItem("sessionid")
 									  + "&reward_id=" + result.result.reward[getId].pk_id
 									  + "&count=" + $(".goodsNum1").val(); 
