b82a85a3eaeedf793027427211edecbe608f5539
Two <605222844@qq.com>
2016-09-16 14:52:11 +0800

    更新channel ID

--- Application/Home/BusinessLogic/Network/TencentIMManager.class.php
+++ Application/Home/BusinessLogic/Network/TencentIMManager.class.php
@@ -15,16 +15,28 @@ class TencentIMManager {
 	$push = new \XingeApp(KeyParam::ACCESS_ID, KeyParam::SECRET_KEY);
 	$message = new \MessageIOS();
 	$message->setExpireTime(86400);
+//	$message->setAlert(array(
+//            'loc-key' => $body,
+//            'loc-args' => $args));
+//	$message->setBadge(1);
+//	$message->setSound("beep.wav");
+//	$custom = array('type' => $type, 'object'=> $object, 
+//            'extra' => $extraMessage);
+//	$message->setCustom($custom);
+//	$acceptTime = new \TimeInterval(0, 0, 23, 59);
+//	$message->addAcceptTime($acceptTime);
+        
 	$message->setAlert(array(
-            'loc-key' => $body,
-            'loc-args' => $args));
+            'loc-key' => "FRIEND_ACCEPT_MESSAGE",
+            'loc-args' => ["向捷"]));
+	//$mess->setAlert(array('key1'=>'value1'));
 	$message->setBadge(1);
 	$message->setSound("beep.wav");
-	$custom = array('type' => $type, 'object'=> $object, 
-            'extra' => $extraMessage);
+	$custom = array('type' => 2, 'object'=> null, 
+            'extra' => 2);
 	$message->setCustom($custom);
-	$acceptTime = new \TimeInterval(0, 0, 23, 59);
-	$message->addAcceptTime($acceptTime);
+	$acceptTime1 = new \TimeInterval(0, 0, 23, 59);
+	$message->addAcceptTime($acceptTime1);
 	$ret = $push->PushSingleAccount(0, $phone, $message, \XingeApp::IOSENV_DEV);
         echo json_encode($ret);
 	return $ret['ret_code'] == 0;
