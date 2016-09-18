a3f1ff120d94ea61b8a534cb4dcf07b6e27f5119
xiangmin123 <270487268@qq.com>
2016-09-18 09:39:19 +0800

    change

--- Public/js/webpnet.js
+++ Public/js/webpnet.js
@@ -401,10 +401,10 @@ $(function(){
 							   		alert(arr);
 							   		alert(arr[0]);
 							   		alert(arr[1]);
-							   		$("#province1 option[text='北京市']").attr("selected", false);
-							   		$("#province1 option[text='天津市']").attr("selected", true); 
-							   		$("#city1 option[text='天津']").attr("selected", true); 
-							   		$("#district1 option[text='河东区']").attr("selected", true); 
+							   		$("#province1 option:contains('天津市')").attr("selected", true);
+//							   		$("#province1 option[text='天津市']").attr("selected", "selected"); 
+//							   		$("#city1 option[text='天津']").attr("selected", true); 
+//							   		$("#district1 option[text='河东区']").attr("selected", true); 
 							   		
 							   		$("#myaddress").val(default_address);
 							   		$("input[name='radioselect']").eq(0).attr("checked","checked");
