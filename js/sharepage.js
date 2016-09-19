9f70771040aae4bdde3865ae4321692a3cd22d0b
xiangmin123 <270487268@qq.com>
2016-09-19 21:12:45 +0800

    change

---
+++ js/sharepage.js
@@ -0,0 +1,26 @@
+$(function(){
+	alert($("#sessionid").val());
+	alert($("#goalid").val());
+	
+	var getGoalById = {
+		url: "/index.php/home/map/getGoalById",
+		type: 'POST',
+		data:$("#goalid").val(),
+		
+		success: function(result, status) {
+			switch(result["code"]){
+				case "10000":
+					sessionStorage["record"] = result["result"];
+					document.getElementById("scoreNum").innerHTML = result["result"];
+					break;
+				case "11000":
+					clearStorage();
+					break;
+			}
+		},
+	};
+	$.ajax(getGoalById);
+	
+	
+	
+});
