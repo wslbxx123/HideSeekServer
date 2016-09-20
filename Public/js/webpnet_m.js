482d5caa15aa3ffda7bb3918ad6b65ea62279adf
xiangmin123 <270487268@qq.com>
2016-09-20 15:51:07 +0800

    change

--- Public/js/webpnet_m.js
+++ Public/js/webpnet_m.js
@@ -253,6 +253,7 @@ $(function(){
 						   		});
 						   		//进入购买支付确认界面
 						   		alert(products_id);
+						   		alert(sessionStorage.getItem("has_guide"));
 						   		if(products_id==2){
 							   		if(sessionStorage.getItem("has_guide")==1){
 							   			var cf = confirm("您已经拥有怪兽图鉴，是否继续购买？")
