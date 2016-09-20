423ea6ace7bfbe2f938b7cccf9e4cd601317d327
Two <605222844@qq.com>
2016-09-20 21:33:53 +0800

    分享页面参数更新

--- Application/Home/Controller/MindexController.class.php
+++ Application/Home/Controller/MindexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Home\Controller\BaseController;
 
 class MindexController extends BaseController {
-    public function Index(){
+    public function index(){
 //        self::setMobileHeader();
         
         $tradeStatus = filter_input(INPUT_GET, 'trade_status');
