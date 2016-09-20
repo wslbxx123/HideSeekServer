efa9d533ce71c2ea8da34c5a2899c72c28bf140d
Two <605222844@qq.com>
2016-09-20 21:26:50 +0800

    分享页面参数更新

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -21,7 +21,7 @@ class BaseController extends Controller {
         
         if($isMobile || !RequestUtil::isSSL($server)) {
              header('Location: https://'.$serverName
-                     .U($controller.ACTION_NAME).$server['QUERY_STRING']);
+                     .U($controller, ACTION_NAME).$server['QUERY_STRING']);
              exit();
         } 
     }
