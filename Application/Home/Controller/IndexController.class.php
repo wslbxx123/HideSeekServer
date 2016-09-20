3b079f48fb1f501616d64965327e852958f0cc1f
Two <605222844@qq.com>
2016-09-20 18:57:28 +0800

    分享页面参数更新

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,10 +1,18 @@
 <?php
 namespace Home\Controller;
 use Home\Controller\BaseController;
+use Home\Common\Util\RequestUtil;
 
 class IndexController extends BaseController {
     public function index(){
         self::setHeader();
+        $server = filter_input_array(INPUT_SERVER);
+        $isMobile = RequestUtil::isMobile($server);
+        
+        if($isMobile) {
+             header('Location: '.U('Mindex/index'));
+             exit();
+        }
         
         $tradeStatus = filter_input(INPUT_GET, 'trade_status');
         
