a384ec449bd606ad8f09578dc99c2c48c4faa95d
xiangmin123 <270487268@qq.com>
2016-09-20 16:13:36 +0800

    change

--- Public/js/webpnet_m.js
+++ Public/js/webpnet_m.js
@@ -252,12 +252,11 @@ $(function(){
 						   			$(".goodsprice").html($(".goodsNum").val()*result.result.products[getId].price+"元");
 						   		});
 						   		//进入购买支付确认界面
-						   		alert(products_id);
-						   		alert(sessionStorage.getItem("has_guide"));
-						   		if(products_id==2){
-						   			alert(1);
-							   		if(sessionStorage.getItem("has_guide")=="1"){
-							   			alert(2);
+//						   		alert(products_id);
+//						   		alert(sessionStorage.getItem("has_guide"));
+						   		if(products_id==2){						   			
+							   		if(sessionStorage.getItem("has_guide")=="1"||sessionStorage.getItem("has_guide")==1){
+					
 							   			var cf = confirm("您已经拥有怪兽图鉴，是否继续购买？")
 							   			if(cf){
 							   				$("#confirmpurchase").fadeIn();
