aed71e69c598b1555106049ad848500ebf65f521
Two <605222844@qq.com>
2016-09-20 18:50:31 +0800

    分享页面参数更新

--- Application/Home/Controller/MindexController.class.php
+++ Application/Home/Controller/MindexController.class.php
@@ -15,11 +15,13 @@ class MindexController extends BaseController {
     public function sharePage(){
         self::setHeader();
         
-        $tradeStatus = filter_input(INPUT_GET, 'trade_status');
-        $sessionId = filter_input(INPUT_GET, 'session_id');
+        $goalId = filter_input(INPUT_GET, 'goal_id');
+        $nickname = filter_input(INPUT_GET, 'nickname');
+        $role = filter_input(INPUT_GET, 'role');
         
-        $this->assign("tradeStatus", $tradeStatus);
-        $this->assign("sessionId", $sessionId);
+        $this->assign("goalId", $goalId);
+        $this->assign("nickname", $nickname);
+        $this->assign("role", $role);
         $this->display();
     }
     
