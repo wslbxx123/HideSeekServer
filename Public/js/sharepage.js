d98eb9eb85e2c81181ac3fffd685c973955dde05
xiangmin123 <270487268@qq.com>
2016-10-02 08:17:47 +0800

    change

--- Public/js/sharepage.js
+++ Public/js/sharepage.js
@@ -70,12 +70,13 @@ $(function(){
     alert(ver);
     
 	btn_open.addEventListener('click', function() {
-//		if(ver>=9){  
-//      	window.location.href = 'https://www.hideseek.cn/home/mindex/sharePage'+'?goal_id='+$("#goalid").val();
-//		}  
-//		else{  
+		if(ver>=9){  
+        	window.location.href = 'https://www.hideseek.cn/home/mindex/sharePage'+'?goal_id='+$("#goalid").val();
+		}  
+		else{
+			alert(1);
 	        window.location.href = 'hideseek://'; 
-//		} 
+		} 
 		setTimeout(function () {
            
 		   if(isAndroid){
