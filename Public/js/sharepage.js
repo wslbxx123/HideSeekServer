9abd5e227f5afb8dd369dd474dbf97c0fc11b45c
xiangmin123 <270487268@qq.com>
2016-10-06 14:37:12 +0800

    change

--- Public/js/sharepage.js
+++ Public/js/sharepage.js
@@ -87,9 +87,6 @@ $(function(){
 			
 			setTimeout(function(){
 	           window.location.href = 'https://itunes.apple.com/us/app/hideseek/id1154398844?ls=1&mt=8';
-	           alert("1");
-	           $("#message").html('跳转失败，请使用浏览器查看页面！'); 
-	           $("#myrole").attr('src',''); 
 	        }, 100);
 	   }
 	   else{
